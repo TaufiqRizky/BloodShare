0ae1dbecf8d6b3fbe62a84e854914689f4ab4fbb
TaufiqRizky <taufiq.it2@gmail.com>
2021-05-01 16:16:33 +0700

    add comment

--- app/src/main/java/com/trizdev/bloodshare/MainActivity.java
+++ app/src/main/java/com/trizdev/bloodshare/MainActivity.java
@@ -3,7 +3,11 @@ package com.trizdev.bloodshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//tanggal :1 april 2021
+//deskripsi : Tugas Aplikasi Bloodshare
+//Nim : 10118080
+//Nama :Taufiq Rizky
+//Kelas : IF 2
 public class MainActivity extends AppCompatActivity {
 
     @Override
